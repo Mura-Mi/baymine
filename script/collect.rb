1706623d87e8705b5d7b831ff6beca0bbb0f0bc9
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-10-13 01:37:30 +0900

    Fix do - end block

--- script/collect.rb
+++ script/collect.rb
@@ -5,12 +5,13 @@ tw = get_twitter
 
 persister = Persister.new
 
-tw.search("ベイスターズ").each |tweet|
-    persister[:tw_test].insert_one({
-                                       id: tweet.id,
-                                       text: tweet.text,
-                                       user: tweet.user.screen_name,
-                                       fav: tweet.favorite_count,
-                                       rt: tweet.retweet_count
-                                   })
+tw.search("ベイスターズ").each do |tweet|
+  persister[:tw_test].insert_one({
+                                     id: tweet.id,
+                                     text: tweet.text,
+                                     user: tweet.user.screen_name,
+                                     fav: tweet.favorite_count,
+                                     rt: tweet.retweet_count
+                                 })
+end
 
