c682feb50be351240d6b089e47cb3e9a0e51bfe2
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-10-13 09:02:47 +0900

    Refactor: create method to determine to persist tweet

--- script/collect.rb
+++ script/collect.rb
@@ -4,10 +4,14 @@ require_relative '../lib/persister'
 tw = get_twitter
 
 persister = Persister.new
+col = persister.driver[:tw_test]
+
+def need_to_persist(tw)
+  !tw.retweet? && col.count({id: tweet.id}) == 0
+end
 
 tw.search("ベイスターズ").each do |tweet|
-  col = persister.driver[:tw_test]
 
-  col.insert_one(BayMine::Tweet.new(tweet).to_hash) unless tweet.retweet? || col.count({id: tweet.id}) > 0
+  col.insert_one(BayMine::Tweet.new(tweet).to_hash) if need_to_persist(tweet)
 end
 
