d01de3844f3c9adc3cac67c242a3de9cf4cabaf5
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-10-13 09:14:07 +0900

    Add logger

--- script/collect.rb
+++ script/collect.rb
@@ -1,8 +1,11 @@
+require 'logger'
+require 'date'
 require_relative '../lib/tw'
 require_relative '../lib/tweet'
 require_relative '../lib/persister'
 
 tw = get_twitter
+logger = Logger.new("log/collect-#{Date.today.strftime('%Y-%m-%d')}.log")
 
 persister = Persister.new
 col = persister.driver[:tw_test]
@@ -11,8 +14,12 @@ def need_to_persist(tw, col)
   !tw.retweet? && col.count({id: tw.id}) == 0
 end
 
-tw.search("ベイスターズ").each do |tweet|
+count = 0
 
+tw.search("ベイスターズ").each do |tweet|
   col.insert_one(BayMine::Tweet.new(tweet).to_hash) if need_to_persist(tweet, col)
+  count += 1
 end
+
+logger.info("Persist #{count} tweets to collection tw_test")
 
