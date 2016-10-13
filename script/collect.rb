c353174753e590d91b68ea8678d367f40e5d97f6
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-10-13 09:06:01 +0900

    Fix collect script

--- script/collect.rb
+++ script/collect.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/tw'
+require_relative '../lib/tweet'
 require_relative '../lib/persister'
 
 tw = get_twitter
@@ -6,12 +7,12 @@ tw = get_twitter
 persister = Persister.new
 col = persister.driver[:tw_test]
 
-def need_to_persist(tw)
-  !tw.retweet? && col.count({id: tweet.id}) == 0
+def need_to_persist(tw, col)
+  !tw.retweet? && col.count({id: tw.id}) == 0
 end
 
 tw.search("ベイスターズ").each do |tweet|
 
-  col.insert_one(BayMine::Tweet.new(tweet).to_hash) if need_to_persist(tweet)
+  col.insert_one(BayMine::Tweet.new(tweet).to_hash) if need_to_persist(tweet, col)
 end
 
