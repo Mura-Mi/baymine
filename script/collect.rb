0a55e7eab52c73f6c990cfa7a7fafe5cc8d3f8ba
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-10-14 01:08:57 +0900

    Limit query times to obey API Limit Documentation

--- script/collect.rb
+++ script/collect.rb
@@ -18,7 +18,9 @@ begin
 
   count = 0
 
-  tw.search("ベイスターズ").each do |tweet|
+  max_id = col.find.sort({id: -1}).limit(1).first[:id]
+
+  tw.search("ベイスターズ", {since_id: max_id}).take(3000).each do |tweet|
     if need_to_persist(tweet, col)
       col.insert_one(BayMine::Tweet.new(tweet).to_hash)
       count += 1
