91e5b324377cb7d5fcd006de848d3fef4fbb86ee
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-10-18 00:20:21 +0900

    Add script to refine analyze script

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -1,16 +1,27 @@
 require_relative('./analyze')
+require 'twitter/tweet'
+require 'bson/document'
 
 module BayMine
   class Tweet
     ANALYZER = BayMine::Analyzer.new
 
     def initialize(tweet)
-      @id = tweet.id
-      @text = tweet.text
-      @user = tweet.user.screen_name
-      @fav = tweet.favorite_count
-      @rt = tweet.retweet_count
-      @created_at = tweet.created_at
+      if tweet.is_a? Twitter::Tweet
+        @id = tweet.id
+        @text = tweet.text
+        @user = tweet.user.screen_name
+        @fav = tweet.favorite_count
+        @rt = tweet.retweet_count
+        @created_at = tweet.created_at
+      elsif tweet.is_a? BSON::Document
+        @id = tweet[:id]
+        @text = tweet[:text]
+        @user = tweet[:user]
+        @fav = tweet[:fav]
+        @rt = tweet[:rt]
+        @created_at = tweet[:created_at]
+      end
     end
 
     def to_hash
---
+++ script/refine_analyze.rb
@@ -0,0 +1,36 @@
+require_relative '../lib/persister'
+require_relative '../lib/analyze'
+require_relative '../lib/tweet'
+require 'logger'
+
+def millsec
+  Time.now.instance_eval { self.to_i * 1000 + (usec / 1000) }
+end
+
+logger = Logger.new("log/analyze-#{Date.today.strftime('%Y-%m-%d')}.log")
+
+start = millsec
+count = 0
+
+begin
+  collection = Persister.new.driver[:tw_test]
+
+  collection.find({
+                      "$or": [
+                          {"keywords": {"$exists": false}},
+                          # TODO improve Sem-Var query
+                          {"keywords.version.patch": {"$lt": BayMine::Analyzer.version[:patch]}}
+                      ]
+                  }).limit(500).each do |tw|
+    collection.update_one({_id: tw[:_id]}, BayMine::Tweet.new(tw).to_hash)
+    count += 1
+  end
+
+
+rescue => e
+  logger.fatal e
+end
+
+completed = millsec
+
+logger.info("Analyzing #{count} tweet(s) has been completed in #{completed - start} msec.")
