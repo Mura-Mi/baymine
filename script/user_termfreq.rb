4d82a369ac1d4ea1cb8df6ace5f03ed6a0b10dd8
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-10-19 09:19:38 +0900

    TF-IDF

--- script/user_termfreq.rb
+++ script/user_termfreq.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/persister'
 require_relative '../lib/utils'
+require 'logger'
 
 p = Persister.new
 users = p.user_repository
@@ -8,19 +9,63 @@ tweets_repo = p.tweet_repository
 user_words = {}
 all_words = {}
 
-users.find.limit(15).each do |user|
-  username = user[:user]
-  words = {}
-   tweets_repo.find({user: username}).each { |t|
-     t[:keywords][:general].each { |k, v|
-       words[k] = words[k].to_i + v
-       all_words[k] = all_words[k].to_i + v
-     }
-  }
-  user_words[username] = BayMine::Utils.normalize(words)
+all_freq = {}
+
+def millsec
+  Time.now.instance_eval { self.to_i * 1000 + (usec / 1000) }
 end
 
-all_words = BayMine::Utils.normalize(all_words)
+logger = Logger.new("log/tf-idf-#{Date.today.strftime('%Y-%m-%d')}.log")
 
-puts all_words
+begin
+  start = millsec
 
+  # Count Appearance of each words
+  users.find.limit(30).each do |user|
+    username = user[:user]
+    words = {}
+    tweets_repo.find({user: username}).each { |t|
+      t[:keywords][:general].each { |k, v|
+        words[k] = words[k].to_i + v
+        all_words[k] = all_words[k].to_i + v
+        all_freq[k] = all_words[k].to_i + 1
+      }
+    }
+    user_words[username] = words
+  end
+
+  # Term Frequency ( log(N)+1 )
+  user_tfs = user_words.map { |user, words|
+    tf = words.map { |word, freq|
+      [word, Math.log10(freq) +1]
+    }.to_h
+    [user, tf]
+  }.to_h
+
+  size = users.count
+
+  user_idfs = user_words.map { |user, words|
+    idf = words.keys.map { |word|
+      [word, size / (all_freq[word].to_i || 1)] # in case of all_freq = nil
+    }.to_h
+    [user, idf]
+  }.to_h
+
+  user_tf_idfs = user_tfs.map { |user, tfs|
+    idfs = user_idfs[user]
+    return if idfs.nil? || idfs.empty?
+    tf_idfs = tfs.map { |word, tf|
+      [word, tf * idfs[word].to_f]
+    }.to_h
+    [user, tf_idfs]
+  }.to_h
+
+  user_tf_idfs.each do |user, tf_idf|
+    users.update_one({user: user}, {"$set": { tf_idf: tf_idf, tf_idf_created_at: Time.now }})
+  end
+
+  fin = millsec
+  logger.info "Refine #{sizes} TF-IDF in #{fin - start} msec."
+rescue => e
+  logger.fatal e
+end
