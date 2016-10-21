eed393af445fc0a4d08fe2ee63413920d54c1c80
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-10-21 23:05:44 +0900

    Cluster analysis with terrible performance

--- script/extract_cluster.rb
+++ script/extract_cluster.rb
@@ -1,5 +1,8 @@
 require_relative '../lib/persister'
 require_relative '../lib/utils'
+require 'logger'
+
+logger = Logger.new(STDOUT)
 
 persister = Persister.new
 
@@ -22,6 +25,11 @@ class GravityBuilder
     end
   end
 
+  def clear
+    @users.clear
+    @tf_idf_sum.clear
+  end
+
   def grav_vector
     count = @users.size
     @tf_idf_sum.map { |word, value|
@@ -37,9 +45,39 @@ users.each do |u|
   builders.sample.add(u)
 end
 
-gravities = builders.map { |b| b.grav_vector }
+logger.debug { builders.map { |b| b.users.count } }
 
-users.each do |u|
-  # Calc distance
+none_moved = false
+
+count = 0
+until none_moved do
+  gravities = builders.map { |b| b.grav_vector }
+  builders.each { |b| b.clear }
+
+  none_moved = true
+
+  users.each do |u|
+    min_distance = nil
+    nearest = nil
+
+    logger.debug u[:user]
+
+    gravities.map.with_index { |g, n_th|
+      distance = BayMine::Utils.calc_distance(g, u[:tf_idf])
+      logger.debug { "#{u[:user]}, #{n_th}" }
+      if min_distance.nil? || min_distance > distance
+        none_moved = false
+        min_distance = distance
+        nearest = n_th
+      end
+    }
+
+    builders[nearest].add(u)
+  end
+
+  count += 1
+  logger.debug { "#{count}th loop end." }
+  logger.debug { builders.map { |b| b.users.count } }
 end
 
+puts builders.map { |b| b.grav_vector }
